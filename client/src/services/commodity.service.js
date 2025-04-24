1dcf528fe9d0a6e35492dd3680269c7d9654fa15
Lana <kainingyang22@gmail.com>
2025-04-24 23:00:58 +0800

    改後端連結

--- client/src/services/commodity.service.js
+++ client/src/services/commodity.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/commodity";
+const API_URL = "https://secondhandclothesserve.onrender.com/api/commodity";
 
 class CommodityService {
   post(title, description, price) {
