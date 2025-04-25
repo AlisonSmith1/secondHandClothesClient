e5badf824c2b5fbac2397ae789e7a5e6f4697822
Lana <kainingyang22@gmail.com>
2025-04-25 18:23:48 +0800

    change word color

--- client/src/components/enroll-component.js
+++ client/src/components/enroll-component.js
@@ -35,75 +35,77 @@ const EnrollComponent = ({ currentUser, setCurrentUser }) => {
   };
 
   return (
-    <div style={{ padding: "3rem" }}>
-      {!currentUser && (
-        <div>
-          <p>您必須先登入才能開始註冊商品。</p>
-          <button
-            className="btn btn-primary btn-lg"
-            onClick={handleTakeToLogin}
-          >
-            回到登入頁面
-          </button>
-        </div>
-      )}
-      {currentUser && currentUser.user.role == "business" && (
-        <div>
-          <h1>只有買家才能夠註冊商品</h1>
-        </div>
-      )}
-      {currentUser && currentUser.user.role == "customer" && (
-        <div className="search input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            onChange={handleChangeInput}
-          />
-          <button onClick={handleSearch} className="btn btn-primary">
-            搜尋商品
-          </button>
-        </div>
-      )}
-      {currentUser && searchResult && searchResult.length != 0 && (
-        <div>
-          <p>這是我們從API返回的數據:</p>
-          {searchResult.map((commodity) => {
-            return (
-              <div
-                key={commodity._id}
-                className="card"
-                style={{ width: "18rem" }}
-              >
-                <div className="card-body">
-                  <h5 className="card-title">商品名稱:{commodity.title}</h5>
-                  <p style={{ margin: "0.5rem 0rem" }} className="card-text">
-                    商品描述: {commodity.description}
-                  </p>
-                  <p style={{ margin: "0.5rem 0rem" }}>
-                    購買人數:
-                    {commodity.customers.length}
-                  </p>
-                  <p style={{ margin: "0.5rem 0rem" }}>
-                    商品價格: {commodity.price}
-                  </p>
-                  <p style={{ margin: "0.5rem 0rem" }}>
-                    賣家: {commodity.business.username}
-                  </p>
+    <div className="bg-threeSection">
+      <div style={{ padding: "3rem" }}>
+        {!currentUser && (
+          <div>
+            <p>您必須先登入才能開始註冊商品。</p>
+            <button
+              className="btn btn-primary btn-lg"
+              onClick={handleTakeToLogin}
+            >
+              回到登入頁面
+            </button>
+          </div>
+        )}
+        {currentUser && currentUser.user.role == "business" && (
+          <div>
+            <h1>只有買家才能夠註冊商品</h1>
+          </div>
+        )}
+        {currentUser && currentUser.user.role == "customer" && (
+          <div className="search input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              onChange={handleChangeInput}
+            />
+            <button onClick={handleSearch} className="btn btn-primary">
+              搜尋商品
+            </button>
+          </div>
+        )}
+        {currentUser && searchResult && searchResult.length != 0 && (
+          <div>
+            <p style={{ color: "white" }}>這是我們從API返回的數據:</p>
+            {searchResult.map((commodity) => {
+              return (
+                <div
+                  key={commodity._id}
+                  className="card"
+                  style={{ width: "18rem" }}
+                >
+                  <div className="card-body">
+                    <h5 className="card-title">商品名稱:{commodity.title}</h5>
+                    <p style={{ margin: "0.5rem 0rem" }} className="card-text">
+                      商品描述: {commodity.description}
+                    </p>
+                    <p style={{ margin: "0.5rem 0rem" }}>
+                      購買人數:
+                      {commodity.customers.length}
+                    </p>
+                    <p style={{ margin: "0.5rem 0rem" }}>
+                      商品價格: {commodity.price}
+                    </p>
+                    <p style={{ margin: "0.5rem 0rem" }}>
+                      賣家: {commodity.business.username}
+                    </p>
 
-                  <a
-                    href="#"
-                    id={commodity._id}
-                    className="card-text btn btn-primary"
-                    onClick={handleEnroll}
-                  >
-                    註冊商品
-                  </a>
+                    <a
+                      href="#"
+                      id={commodity._id}
+                      className="card-text btn btn-primary"
+                      onClick={handleEnroll}
+                    >
+                      註冊商品
+                    </a>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- client/src/components/postCommodity-component.js
+++ client/src/components/postCommodity-component.js
@@ -34,65 +34,67 @@ const PostCommodityComponent = ({ currentUser, setCurrentUser }) => {
   };
 
   return (
-    <div className="container py-5 ">
-      <div style={{ padding: "3rem" }}>
-        {!currentUser && (
-          <div>
-            <p>在發布商品之前，您必須先登錄。</p>
-            <button
-              className="btn btn-primary btn-lg"
-              onClick={handleTakeToLogin}
-            >
-              帶我進入登錄頁面。
-            </button>
-          </div>
-        )}
-        {currentUser && currentUser.user.role !== "business" && (
-          <div>
-            <p>只有講師可以發布新商品。</p>
-          </div>
-        )}
-        {currentUser && currentUser.user.role == "business" && (
-          <div className="form-group">
-            <label htmlFor="exampleforTitle">商品標題：</label>
-            <input
-              name="title"
-              type="text"
-              className="form-control"
-              id="exampleforTitle"
-              onChange={handleChangeTitle}
-            />
-            <br />
-            <label htmlFor="exampleforContent">內容：</label>
-            <textarea
-              className="form-control"
-              id="exampleforContent"
-              aria-describedby="emailHelp"
-              name="content"
-              onChange={handleChangeDesciption}
-            />
-            <br />
-            <label htmlFor="exampleforPrice">價格：</label>
-            <input
-              name="price"
-              type="number"
-              className="form-control"
-              id="exampleforPrice"
-              onChange={handleChangePrice}
-            />
-            <br />
-            <button className="btn btn-primary" onClick={postCommodity}>
-              交出表單
-            </button>
-            <br />
-            <br />
-            {message && (
-              <div className="alert alert-warning" role="alert">
-                {message}
-              </div>
-            )}
-          </div>
-        )}
+    <div className="bg-threeSection">
+      <div className="container py-5 ">
+        <div style={{ padding: "3rem", color: "white" }}>
+          {!currentUser && (
+            <div>
+              <p>在發布商品之前，您必須先登錄。</p>
+              <button
+                className="btn btn-primary btn-lg"
+                onClick={handleTakeToLogin}
+              >
+                帶我進入登錄頁面。
+              </button>
+            </div>
+          )}
+          {currentUser && currentUser.user.role !== "business" && (
+            <div>
+              <p>只有講師可以發布新商品。</p>
+            </div>
+          )}
+          {currentUser && currentUser.user.role == "business" && (
+            <div className="form-group">
+              <label htmlFor="exampleforTitle">商品標題：</label>
+              <input
+                name="title"
+                type="text"
+                className="form-control"
+                id="exampleforTitle"
+                onChange={handleChangeTitle}
+              />
+              <br />
+              <label htmlFor="exampleforContent">內容：</label>
+              <textarea
+                className="form-control"
+                id="exampleforContent"
+                aria-describedby="emailHelp"
+                name="content"
+                onChange={handleChangeDesciption}
+              />
+              <br />
+              <label htmlFor="exampleforPrice">價格：</label>
+              <input
+                name="price"
+                type="number"
+                className="form-control"
+                id="exampleforPrice"
+                onChange={handleChangePrice}
+              />
+              <br />
+              <button className="btn btn-primary" onClick={postCommodity}>
+                交出表單
+              </button>
+              <br />
+              <br />
+              {message && (
+                <div className="alert alert-warning" role="alert">
+                  {message}
+                </div>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
